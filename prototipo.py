24301ee5a96e60d965edc3263f01970632050e58
Kleber Montenegro <montenegrokleber7@gmail.com>
2026-09-03 21:19:17 -0400

    feat: prototipo funcional de solicitações

--- prototipo.py
+++ prototipo.py
@@ -3,7 +3,7 @@ from datetime import datetime
 def aceitar_solicitacao(solicitacao): #define uma função para aceitar a solicitação
     if solicitacao ["status"] == "AGUARDANDO":
         solicitacao ["status"] = "EM ATENDIMENTO"
-        solicitacao ["horario_de_atendimento"] = datetime.now().strftime("%H:%M")
+        solicitacao ["horario_de_atendimento"] = datetime.now().strftime("%H:%M:%S")
     else:
         print ("Não é possível aceitar essa solicitação")
         print (f"Status da solicitação: {solicitacao['status']}")
@@ -11,26 +11,41 @@ def aceitar_solicitacao(solicitacao): #define uma função para aceitar a solici
 def concluir_solicitacao(solicitacao):#define uma função para concluir a solicitação
     if solicitacao ["status"] == "EM ATENDIMENTO":
         solicitacao ["status"] = "CONCLUÍDO"
-        solicitacao ["horario_de_conclusao"] = datetime.now().strftime("%H:%M")
+        solicitacao ["horario_de_conclusao"] = datetime.now().strftime("%H:%M:%S")
     else:
         print ("Não é possível concluir essa solicitação")
         print (f"Status da solicitação:{solicitacao['status']}")
 
 def exibir_solicitacao (solicitacao): #define uma função apenas para exibir a solicitação
     print ("=== SOLICITAÇÃO ===")
+    print ("ID da solicitação")
     print (solicitacao["id"])
-    print (solicitacao ["alocacao"]["id"])
-    print (solicitacao ["alocacao"]["professor"]["nome"])
-    print (solicitacao ["alocacao"]["turma"]["codigo_turma"])
-    print (solicitacao ["alocacao"]["disciplina"]["nome"])
-    print (solicitacao ["alocacao"]["sala"]["identificacao"])
-    print (solicitacao ["alocacao"]["data_inicio"])
-    print (solicitacao ["alocacao"]["data_fim"])
+    print ("Problema:")
     print (solicitacao ["problema"])
+    print ("Horário de abertura:")
     print (solicitacao ["horario_de_abertura"])
+    print ("Horário de atendimento:")
     print (solicitacao ["horario_de_atendimento"])
+    print ("Horário de conclusão:")
     print (solicitacao ["horario_de_conclusao"])
+    print ("Status da demanda:")
     print (solicitacao ["status"])
+
+def criar_solicitacao(id, alocacao, problema): #define uma função para criar solicitações
+    horario_de_abertura = datetime.now().strftime ("%H:%M:%S")
+    horario_de_atendimento = None
+    horario_de_conclusao = None
+    status = "AGUARDANDO"
+    nova_solicitacao = {
+        "id" : id,
+        "alocacao" : alocacao,
+        "problema" : problema,
+        "horario_de_abertura" : horario_de_abertura,
+        "horario_de_atendimento" : horario_de_atendimento,
+        "horario_de_conclusao" : horario_de_conclusao,
+        "status" : status,
+    }
+    return nova_solicitacao
 
 professor = {
     "id": 1,
@@ -63,47 +78,24 @@ alocacao = {
     "data_inicio": "26/08/2026",
     "data_fim": "10/09/2026"
 }
+while True:
+    try:
+        opcao = int(input("Informe o problema 1.Ar-Condicionado | 2.Projetor | 3.Material\n"))
+        if opcao == 1 or opcao== 2 or opcao == 3:
+            break
+        else:
+            print("Opção inválida")
+    except ValueError:
+        print ("DIGITE APENAS NÚMEROS!")
+        continue
+    
+if opcao == 1:
+    problema = "Ar-condicionado"
+elif opcao == 2:
+    problema = "Projetor"
+elif opcao == 3:
+    problema = "Material"
 
-solicitacao ={
-    "id" : 1,
-    "alocacao": alocacao,
-    "problema": "Ar-condicionado",
-    "horario_de_abertura" : "18:35",
-    "horario_de_atendimento": None,
-    "horario_de_conclusao": None,
-    "status": "AGUARDANDO"
-}
-#estrutura de dicionários para consulta de dados
-print (professor["nome"])
-print (turma["codigo_turma"])
-print (disciplina["nome"])
-print (sala["identificacao"])
-
-print ("=== ALOCAÇÕES ===")
-print (alocacao["id"])
-print (alocacao["professor"]["nome"]) #exibe as informações fazendo a consulta de um dicionário através de outro dicionário
-print (alocacao["turma"]["codigo_turma"])
-print (alocacao["disciplina"]["nome"])
-print (alocacao["sala"]["identificacao"])
-print (alocacao["data_inicio"])
-print (alocacao["data_fim"])
-
-print ("=== SOLICITAÇÃO ===")
-print (solicitacao["id"])
-print (solicitacao ["alocacao"]["id"])
-print (solicitacao ["alocacao"]["professor"]["nome"])
-print (solicitacao ["alocacao"]["turma"]["codigo_turma"])
-print (solicitacao ["alocacao"]["disciplina"]["nome"])
-print (solicitacao ["alocacao"]["sala"]["identificacao"])
-print (solicitacao ["alocacao"]["data_inicio"])
-print (solicitacao ["alocacao"]["data_fim"])
-print (solicitacao ["problema"])
-print (solicitacao ["horario_de_abertura"])
-print (solicitacao ["horario_de_atendimento"])
-print (solicitacao ["horario_de_conclusao"])
-print (solicitacao ["status"])
-
+solicitacao = criar_solicitacao(1, alocacao, problema)
 aceitar_solicitacao(solicitacao)
-exibir_solicitacao(solicitacao)
-concluir_solicitacao(solicitacao)
 exibir_solicitacao(solicitacao)
